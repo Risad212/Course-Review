c769b5a89dc4135829ce3ea4e963cf898109723b
Hafez Risad <83496132+Risad212@users.noreply.github.com>
2026-05-19 15:18:19 +0600

    Add CourseReviewsDB class for course review management

---
+++ inc/Database/CourseReviewsDB.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace LearnPress\CourseReview\Database;
+
+use Exception;
+use LP_Database;
+
+
+class CourseReviewsDB extends LP_Database{
+  	private static $_instance;
+	public $tb_comments;
+	public $tb_commentmeta;
+
+    protected function __construct() {
+		global $wpdb;
+		$prefix               = $wpdb->prefix;
+		$this->tb_comments    = $prefix . 'comments';
+		$this->tb_commentmeta = $prefix . 'commentmeta';
+		parent::__construct();
+	}
+
+    public static function getInstance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+    /**
+     * Get total rating of a course.
+     */
+    public function count_rating_of_course( int $course_id = 0 ){
+         $query = $this->wpdb->prepare(
+           "SELECT SUM(cm.meta_key = '_lpr_ratings') AS total,
+            SUM(cm.meta_value = 5) AS five,
+            SUM(cm.meta_value = 4) AS four,
+            SUM(cm.meta_value = 3) AS three,
+            SUM(cm.meta_value = 2) AS two,
+            SUM(cm.meta_value = 1) AS one
+            FROM {$this->tb_comments} AS c
+            INNER JOIN {$this->tb_commentmeta} AS cm
+            ON c.comment_ID = cm.comment_id
+            INNER JOIN {$this->tb_users} u ON u.ID = c.user_id
+            WHERE c.comment_post_ID = %d
+            AND c.comment_approved = 1
+            AND cm.meta_key = '_lpr_rating'
+           ",
+           $course_id
+         );
+
+         $result = $this->wpdb->get_row($query);
+
+         $this->check_execute_has_error();
+
+         return $result;
+    }
+}
